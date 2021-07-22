393ddaa037d3c62b955eb919c292faf7c1336161
skanda-huayan <torquewow@gmail.com>
2021-07-22 10:22:22 +0800

    Update Main.h
    添加大智慧輸出函數

--- src/Main.h
+++ src/Main.h
@@ -24,6 +24,22 @@ extern "C"
 #endif //__cplusplus
 
 __declspec(dllexport) BOOL RegisterTdxFunc(PluginTCalcFuncInfo **pInfo);
+ 
+/********************************************************************/
+//************************交易师 大智慧******************************//
+/********************************************************************/
+__declspec(dllexport) int WINAPI RUNMODE();
+__declspec(dllexport) int WINAPI KXFX(CALCINFO* pData);     // K线方向
+__declspec(dllexport) int WINAPI KXBH(CALCINFO* pData);     // K线包含
+__declspec(dllexport) int WINAPI KXBH2VAR(CALCINFO* pData); // K线包含处理
+__declspec(dllexport) int WINAPI KXBHG(CALCINFO* pData);    // K线包含处理后的高点
+__declspec(dllexport) int WINAPI KXBHD(CALCINFO* pData);    // K线包含处理后的低点
+__declspec(dllexport) int WINAPI BI(CALCINFO* pData);       // 笔端点
+__declspec(dllexport) int WINAPI DUANVAR(CALCINFO* pData);  // 段端点
+__declspec(dllexport) int WINAPI ZSZGVAR(CALCINFO* pData);  // 中枢高
+__declspec(dllexport) int WINAPI ZSZDVAR(CALCINFO* pData);  // 中枢低点
+__declspec(dllexport) int WINAPI ZSSEVAR(CALCINFO* pData);  // 中枢开始结束
+__declspec(dllexport) int WINAPI ZSFXVAR(CALCINFO* pData);  // 中枢方向
 
 #ifdef __cplusplus
 }
