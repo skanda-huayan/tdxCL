fa5747e40e8b0978f79355a772e04c99dd6b901e
skanda-huayan <torquewow@gmail.com>
2021-07-22 15:13:14 +0800

    Update Main.h

--- src/Main.h
+++ src/Main.h
@@ -24,135 +24,135 @@ extern "C"
 #endif //__cplusplus
 
 ///////////////////////////////////////////////////////////////////////////
-//·ÖÎöÖÜÆÚ
+//分析周期
 enum DATA_TYPE
 {
-	TICK_DATA=2,				//·Ö±Ê³É½»
-	MIN1_DATA,					//1·ÖÖÓÏß
-	MIN5_DATA,					//5·ÖÖÓÏß					
-	MIN15_DATA,					//15·ÖÖÓÏß
-	MIN30_DATA,					//30·ÖÖÓÏß
-	MIN60_DATA,					//60·ÖÖÓÏß
-	DAY_DATA,					//ÈÕÏß
-	WEEK_DATA,					//ÖÜÏß
-	MONTH_DATA,					//ÔÂÏß
-	MULTI_DATA					//¶àÈÕÏß
+	TICK_DATA=2,				//分笔成交
+	MIN1_DATA,					//1分钟线
+	MIN5_DATA,					//5分钟线					
+	MIN15_DATA,					//15分钟线
+	MIN30_DATA,					//30分钟线
+	MIN60_DATA,					//60分钟线
+	DAY_DATA,					//日线
+	WEEK_DATA,					//周线
+	MONTH_DATA,					//月线
+	MULTI_DATA					//多日线
 };
 
 ///////////////////////////////////////////////////////////////////////////
-//»ù±¾Êý¾Ý
+//基本数据
 
 typedef struct tagSTKDATA	
 {
-	time_t	m_time;			//Ê±¼ä,UCT
-	float	m_fOpen;		//¿ªÅÌ
-	float	m_fHigh;		//×î¸ß
-	float	m_fLow;			//×îµÍ
-	float	m_fClose;		//ÊÕÅÌ
-	float	m_fVolume;		//³É½»Á¿
-	float	m_fAmount;		//³É½»¶î
-	WORD	m_wAdvance;		//ÉÏÕÇ¼ÒÊý(½ö´óÅÌÓÐÐ§)
-	WORD	m_wDecline;		//ÏÂµø¼ÒÊý(½ö´óÅÌÓÐÐ§)
+	time_t	m_time;			//时间,UCT
+	float	m_fOpen;		//开盘
+	float	m_fHigh;		//最高
+	float	m_fLow;			//最低
+	float	m_fClose;		//收盘
+	float	m_fVolume;		//成交量
+	float	m_fAmount;		//成交额
+	WORD	m_wAdvance;		//上涨家数(仅大盘有效)
+	WORD	m_wDecline;		//下跌家数(仅大盘有效)
 } STKDATA;
 
 
 ////////////////////////////////////////////////////////////////////////////
-//À©Õ¹Êý¾Ý,ÓÃÓÚÃèÊö·Ö±Ê³É½»Êý¾ÝµÄÂòÂôÅÌ
+//扩展数据,用于描述分笔成交数据的买卖盘
 
 typedef union tagSTKDATAEx
 {
 	struct
 	{
-		float m_fBuyPrice[3];		//Âò1--Âò3¼Û
-		float m_fBuyVol[3];			//Âò1--Âò3Á¿
-		float m_fSellPrice[3];		//Âô1--Âô3¼Û	
-		float m_fSellVol[3];		//Âô1--Âô3Á¿
+		float m_fBuyPrice[3];		//买1--买3价
+		float m_fBuyVol[3];			//买1--买3量
+		float m_fSellPrice[3];		//卖1--卖3价	
+		float m_fSellVol[3];		//卖1--卖3量
 	};
-	float m_fDataEx[12];			//±£Áô
+	float m_fDataEx[12];			//保留
 } STKDATAEx;
 
 /////////////////////////////////////////////////////////////////////////////
-/*²ÆÎñÊý¾ÝË³Ðò(m_pfFinDataÄÚÈÝ)
+/*财务数据顺序(m_pfFinData内容)
 
-	ÐòºÅ	ÄÚÈÝ
+	序号	内容
 
-	0	×Ü¹É±¾(Íò¹É),
-	1	¹ú¼Ò¹É,
-	2	·¢ÆðÈË·¨ÈË¹É,
-	3	·¨ÈË¹É,
-	4	B¹É,
-	5	H¹É,
-	6	Á÷Í¨A¹É,
-	7	Ö°¹¤¹É,
-	8	A2×ªÅä¹É,
-	9	×Ü×Ê²ú(Ç§Ôª),
-	10	Á÷¶¯×Ê²ú,
-	11	¹Ì¶¨×Ê²ú,
-	12	ÎÞÐÎ×Ê²ú,
-	13	³¤ÆÚÍ¶×Ê,
-	14	Á÷¶¯¸ºÕ®,
-	15	³¤ÆÚ¸ºÕ®,
-	16	×Ê±¾¹«»ý½ð,
-	17	Ã¿¹É¹«»ý½ð,
-	18	¹É¶«È¨Òæ,
-	19	Ö÷ÓªÊÕÈë,
-	20	Ö÷ÓªÀûÈó,
-	21	ÆäËûÀûÈó,
-	22	ÓªÒµÀûÈó,
-	23	Í¶×ÊÊÕÒæ,
-	24	²¹ÌùÊÕÈë,
-	25	ÓªÒµÍâÊÕÖ§,
-	26	ÉÏÄêËðÒæµ÷Õû,
-	27	ÀûÈó×Ü¶î,
-	28	Ë°ºóÀûÈó,
-	29	¾»ÀûÈó,
-	30	Î´·ÖÅäÀûÈó,
-	31	Ã¿¹ÉÎ´·ÖÅä,
-	32	Ã¿¹ÉÊÕÒæ,
-	33	Ã¿¹É¾»×Ê²ú,
-	34	µ÷ÕûÃ¿¹É¾»×Ê,
-	35	¹É¶«È¨Òæ±È,
-	36	¾»×ÊÊÕÒæÂÊ
+	0	总股本(万股),
+	1	国家股,
+	2	发起人法人股,
+	3	法人股,
+	4	B股,
+	5	H股,
+	6	流通A股,
+	7	职工股,
+	8	A2转配股,
+	9	总资产(千元),
+	10	流动资产,
+	11	固定资产,
+	12	无形资产,
+	13	长期投资,
+	14	流动负债,
+	15	长期负债,
+	16	资本公积金,
+	17	每股公积金,
+	18	股东权益,
+	19	主营收入,
+	20	主营利润,
+	21	其他利润,
+	22	营业利润,
+	23	投资收益,
+	24	补贴收入,
+	25	营业外收支,
+	26	上年损益调整,
+	27	利润总额,
+	28	税后利润,
+	29	净利润,
+	30	未分配利润,
+	31	每股未分配,
+	32	每股收益,
+	33	每股净资产,
+	34	调整每股净资,
+	35	股东权益比,
+	36	净资收益率
 */
 
 /////////////////////////////////////////////////////////////////////////////
-//º¯ÊýÊý¾Ý½á¹¹
+//函数数据结构
 
 typedef struct tagCALCINFO
 {
-	const DWORD			m_dwSize;				//½á¹¹´óÐ¡
-	const DWORD			m_dwVersion;			//µ÷ÓÃÈí¼þ°æ±¾(V2.10 : 0x210)
-	const DWORD			m_dwSerial;				//µ÷ÓÃÈí¼þÐòÁÐºÅ
-	const char*			m_strStkLabel;			//¹ÉÆ±´úÂë
-	const BOOL			m_bIndex;				//´óÅÌ
+	const DWORD			m_dwSize;				//结构大小
+	const DWORD			m_dwVersion;			//调用软件版本(V2.10 : 0x210)
+	const DWORD			m_dwSerial;				//调用软件序列号
+	const char*			m_strStkLabel;			//股票代码
+	const BOOL			m_bIndex;				//大盘
 
-	const int			m_nNumData;				//Êý¾ÝÊýÁ¿(pData,pDataEx,pResultBufÊý¾ÝÊýÁ¿)
-	const STKDATA*		m_pData;				//³£¹æÊý¾Ý,×¢Òâ:µ±m_nNumData==0Ê±¿ÉÄÜÎª NULL
-	const STKDATAEx*	m_pDataEx;				//À©Õ¹Êý¾Ý,·Ö±Ê³É½»ÂòÂôÅÌ,×¢Òâ:¿ÉÄÜÎª NULL
+	const int			m_nNumData;				//数据数量(pData,pDataEx,pResultBuf数据数量)
+	const STKDATA*		m_pData;				//常规数据,注意:当m_nNumData==0时可能为 NULL
+	const STKDATAEx*	m_pDataEx;				//扩展数据,分笔成交买卖盘,注意:可能为 NULL
 
-	const int			m_nParam1Start;			//²ÎÊý1ÓÐÐ§Î»ÖÃ
-	const float*		m_pfParam1;				//µ÷ÓÃ²ÎÊý1	
-	const float*		m_pfParam2;				//µ÷ÓÃ²ÎÊý2
-	const float*		m_pfParam3;				//µ÷ÓÃ²ÎÊý3
-	const float*		m_pfParam4;				//µ÷ÓÃ²ÎÊý3
+	const int			m_nParam1Start;			//参数1有效位置
+	const float*		m_pfParam1;				//调用参数1	
+	const float*		m_pfParam2;				//调用参数2
+	const float*		m_pfParam3;				//调用参数3
+	const float*		m_pfParam4;				//调用参数3
 
-	float*				m_pResultBuf;			//½á¹û»º³åÇø
-	const DATA_TYPE		m_dataType;				//Êý¾ÝÀàÐÍ
-	const float*		m_pfFinData;			//²ÆÎñÊý¾Ý
+	float*				m_pResultBuf;			//结果缓冲区
+	const DATA_TYPE		m_dataType;				//数据类型
+	const float*		m_pfFinData;			//财务数据
 } CALCINFO;
 
 /* 
-×¢: 
-	1.º¯Êýµ÷ÓÃ²ÎÊýÓÉm_pfParam1--m_pfParam4´øÈë,ÈôÎªNULLÔò±íÊ¾¸Ã²ÎÊýÎÞÐ§.
-	2.µ±Ò»¸ö²ÎÊýÎÞÐ§Ê±,ÔòÆäºóµÄËùÓÐ²ÎÊý¾ùÎÞÐ§.
-		Èç:m_pfParam2ÎªNULL,Ôòm_pfParam3,m_pfParam4Ò»¶¨ÎªNULL.
-	3.²ÎÊý1¿ÉÒÔÊÇ³£Êý²ÎÊý»òÐòÁÐÊý²ÎÊý,ÆäÓà²ÎÊýÖ»ÄÜÎª³£Êý²ÎÊý.
-	4.Èôm_nParam1Start<0, Ôò²ÎÊý1Îª³£Êý²ÎÊý,²ÎÊýµÈÓÚ*m_pfParam1;
-	5.Èôm_nParam1Start>=0,Ôò²ÎÊý1ÎªÐòÁÐÊý²ÎÊý,m_pfParam1Ö¸ÏòÒ»¸ö¸¡µãÐÍÊý×é,
-		Êý×é´óÐ¡Îªm_nNumData,Êý¾ÝÓÐÐ§·¶Î§Îªm_nParam1Start--m_nNumData.
-		ÔÚÊ±¼äÉÏm_pData[x] Óë m_pfParam1[x]ÊÇÒ»ÖÂµÄ
+注: 
+	1.函数调用参数由m_pfParam1--m_pfParam4带入,若为NULL则表示该参数无效.
+	2.当一个参数无效时,则其后的所有参数均无效.
+		如:m_pfParam2为NULL,则m_pfParam3,m_pfParam4一定为NULL.
+	3.参数1可以是常数参数或序列数参数,其余参数只能为常数参数.
+	4.若m_nParam1Start<0, 则参数1为常数参数,参数等于*m_pfParam1;
+	5.若m_nParam1Start>=0,则参数1为序列数参数,m_pfParam1指向一个浮点型数组,
+		数组大小为m_nNumData,数据有效范围为m_nParam1Start--m_nNumData.
+		在时间上m_pData[x] 与 m_pfParam1[x]是一致的
 */
-    
+
 __declspec(dllexport) BOOL RegisterTdxFunc(PluginTCalcFuncInfo **pInfo);
  
 /********************************************************************/
